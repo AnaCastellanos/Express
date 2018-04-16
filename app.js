e13f11d13c54ac3736517cc126e6d3c13be83340
Ana Castellanos <ana.lec15@gmail.com>
2018-04-16 16:14:58 -0500

    Ejemplo

---
+++ app.js
@@ -0,0 +1,15 @@
+//Levanta un servidor con express utilizando rutas en el puerto 3000
+
+
+const express = require('express');
+const app = express(); 
+
+app.use(express.static('public')); //Express utiliza los recursos estaticos de public
+
+// app.get('/', (req, res) => { //Utiliza express para manejar las rutas
+// 	res.end('Works');//Respuesta end: Muestra en pantalla 'works'
+// });
+
+app.listen(3000, () => {
+	console.log('Servidor iniciado en el puerto 3000');
+});
