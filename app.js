162704c32bf9f2bd8bc9549a32d83e113edde5e7
Ana Castellanos <ana.lec15@gmail.com>
2018-04-16 16:44:13 -0500

    Estilos y versión de bootstrap

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const express = require('express');
 const app = express(); 
 
-app.use(express.static('public')); //Express utiliza los recursos estaticos de public
+app.use(express.static('public'));  //Express utiliza los recursos estaticos de public
 
 // app.get('/', (req, res) => { //Utiliza express para manejar las rutas
 // 	res.end('Works');//Respuesta end: Muestra en pantalla 'works'
